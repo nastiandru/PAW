c9cba1cdbaf02cfdfd0ddda4db96a105a691993c
nastiandru <nastia.andr070703@gmail.com>
2023-06-20 00:45:37 +0300

    Update tasks.component.ts

--- ManageMe/src/app/tasks/tasks.component.ts
+++ ManageMe/src/app/tasks/tasks.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Feature, Task } from '../features/feature.model';
+import { FeaturesService } from '../services/features.service';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.scss']
 })
-export class TasksComponent {
+export class TasksComponent implements OnInit {
+  features: Feature[] = [];
+  tasks: Task[] = [];
+  selectedFeatureId: number = 0;
 
+  constructor(private featuresService: FeaturesService) { }
+
+  ngOnInit(): void {
+    this.loadFeatures();
+  }
+
+  loadFeatures(): void {
+    this.featuresService.getFeatures().subscribe(
+      (features: Feature[]) => {
+        this.features = features;
+      },
+      (error) => {
+        console.log('Error retrieving features:', error);
+      }
+    );
+  }
+
+  loadTasks(event: any): void {
+    const featureId = event.target.value;
+    this.selectedFeatureId = featureId;
+    this.tasks = this.features.find(f => f.id === featureId)?.tasks || [];
+  }
 }
