5e3a6ddbd83f75390753c4d78e45548af6166358
nastiandru <nastia.andr070703@gmail.com>
2023-06-20 00:44:41 +0300

    Update features.component.ts

--- ManageMe/src/app/features/features.component.ts
+++ ManageMe/src/app/features/features.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Feature } from './feature.model';
+import { FeaturesService } from '../services/features.service';
 
 @Component({
   selector: 'app-features',
   templateUrl: './features.component.html',
   styleUrls: ['./features.component.scss']
 })
-export class FeaturesComponent {
+export class FeaturesComponent implements OnInit {
+  features: Feature[] = [];
+  newFeatureName: string = '';
 
+  constructor(private featuresService: FeaturesService) { }
+
+  ngOnInit(): void {
+    this.loadFeatures();
+  }
+
+  loadFeatures(): void {
+    this.featuresService.getFeatures().subscribe(
+      (features: Feature[]) => {
+        this.features = features;
+      },
+      (error) => {
+        console.log('Error retrieving features:', error);
+      }
+    );
+  }
+
+  addFeature(): void {
+    const newFeature: Feature = {
+      id: Date.now(),
+      name: this.newFeatureName,
+      tasks: []
+    };
+
+    this.featuresService.addFeature(newFeature).subscribe(
+      () => {
+        this.loadFeatures();
+        this.newFeatureName = '';
+      },
+      (error) => {
+        console.log('Error adding feature:', error);
+      }
+    );
+  }
+
+  deleteFeature(featureId: number): void {
+    this.featuresService.deleteFeature(featureId).subscribe(
+      () => {
+        this.loadFeatures();
+      },
+      (error) => {
+        console.log('Error deleting feature:', error);
+      }
+    );
+  }
 }
